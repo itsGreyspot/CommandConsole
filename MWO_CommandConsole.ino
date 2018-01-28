91e2da20f16300f5e67a239b55a1436eb9b22d1f
itsGreyspot <itsGreyspot@gmail.com>
2018-01-28 11:10:40 -0600

    MWO_CommandConsole

---
+++ MWO_CommandConsole.ino
@@ -0,0 +1,178 @@
+
+
+/*
+* Requires Joystick Library v1.0 
+*   https://github.com/MHeironimus/ArduinoJoystickLibrary/tree/version-1.0
+* Requires Keypad Library 
+*   http://playground.arduino.cc/Code/Keypad
+* Optional: Requires LiquidCrystal I2C
+*   https://github.com/marcoschwartz/LiquidCrystal_I2C
+* 
+* Thanks to: 
+*  AMSTUDIO 
+*   https://www.patreon.com/AMSTUDIO
+*   https://www.youtube.com/watch?v=Z7Sc4MJ8RPM&t=169s
+*/
+
+//#include <LiquidCrystal_I2C.h>
+
+#include <Joystick.h>
+#include <Keypad.h>
+
+#define ENABLE_PULLUPS
+#define NUMROTARIES 2
+
+//LiquidCrystal_I2C lcd(0x27,20,4);
+
+struct rotariesdef {
+  byte pin1;
+  byte pin2;
+  int ccwchar;
+  int cwchar;
+  volatile unsigned char state;
+};
+
+const byte ROWS = 5; //four rows
+const byte COLS = 5; //four columns
+
+byte keys[ROWS][COLS] = {
+  {0, 1, 2, 3, 4},
+  {5, 6, 7, 8, 9},
+  {10, 11, 12, 13, 14},
+  {15, 16, 17, 18, 19 },
+  {20, 21, 22, 23, 24}
+};
+
+rotariesdef rotaries[NUMROTARIES] {
+  {4,5,25,26,0},
+  {6,7,27,28,0},
+};
+
+#define DIR_CCW 0x10
+#define DIR_CW 0x20
+#define R_START 0x0
+
+#ifdef HALF_STEP
+#define R_CCW_BEGIN 0x1
+#define R_CW_BEGIN 0x2
+#define R_START_M 0x3
+#define R_CW_BEGIN_M 0x4
+#define R_CCW_BEGIN_M 0x5
+const unsigned char ttable[6][4] = {
+  // R_START (00)
+  {R_START_M,            R_CW_BEGIN,     R_CCW_BEGIN,  R_START},
+  // R_CCW_BEGIN
+  {R_START_M | DIR_CCW, R_START,        R_CCW_BEGIN,  R_START},
+  // R_CW_BEGIN
+  {R_START_M | DIR_CW,  R_CW_BEGIN,     R_START,      R_START},
+  // R_START_M (11)
+  {R_START_M,            R_CCW_BEGIN_M,  R_CW_BEGIN_M, R_START},
+  // R_CW_BEGIN_M
+  {R_START_M,            R_START_M,      R_CW_BEGIN_M, R_START | DIR_CW},
+  // R_CCW_BEGIN_M
+  {R_START_M,            R_CCW_BEGIN_M,  R_START_M,    R_START | DIR_CCW},
+};
+#else
+#define R_CW_FINAL 0x1
+#define R_CW_BEGIN 0x2
+#define R_CW_NEXT 0x3
+#define R_CCW_BEGIN 0x4
+#define R_CCW_FINAL 0x5
+#define R_CCW_NEXT 0x6
+
+const unsigned char ttable[7][4] = {
+  // R_START
+  {R_START,    R_CW_BEGIN,  R_CCW_BEGIN, R_START},
+  // R_CW_FINAL
+  {R_CW_NEXT,  R_START,     R_CW_FINAL,  R_START | DIR_CW},
+  // R_CW_BEGIN
+  {R_CW_NEXT,  R_CW_BEGIN,  R_START,     R_START},
+  // R_CW_NEXT
+  {R_CW_NEXT,  R_CW_BEGIN,  R_CW_FINAL,  R_START},
+  // R_CCW_BEGIN
+  {R_CCW_NEXT, R_START,     R_CCW_BEGIN, R_START},
+  // R_CCW_FINAL
+  {R_CCW_NEXT, R_CCW_FINAL, R_START,     R_START | DIR_CCW},
+  // R_CCW_NEXT
+  {R_CCW_NEXT, R_CCW_FINAL, R_CCW_BEGIN, R_START},
+};
+#endif
+
+byte rowPins[ROWS] = {21,20,19,18,15}; //connect to the row pinouts of the kpd  /*A3, A2, A1, A0, 15*/
+byte colPins[COLS] = {14,16,10,9,8}; //connect to the column pinouts of the kpd /*14,16,10,9,8*/
+
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup() {
+  //lcd.init();
+  //lcd.backlight();
+  Joystick.begin();
+  rotary_init();
+}  // End setup()
+
+
+void loop() {
+
+
+  CheckAllEncoders();
+  CheckAllButtons();
+
+}  // End loop()
+
+void CheckAllButtons(void) {
+  // Fills kpd.key[ ] array with up-to 10 active keys.
+  // Returns true if there are ANY active keys.
+  if (kpd.getKeys())
+  {
+    for (int i = 0; i < LIST_MAX; i++) // Scan the whole key list.
+    {
+      if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
+      {
+        switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+          case PRESSED:
+            
+          case HOLD:
+            
+            Joystick.setButton(kpd.key[i].kchar , 1);
+            break;
+          case RELEASED:
+                       
+          case IDLE:
+            Joystick.setButton(kpd.key[i].kchar, 0);
+            break;
+        }
+
+
+      }
+    }
+  }
+}
+
+void rotary_init() {
+  for (int i=0;i<NUMROTARIES;i++) {
+    pinMode(rotaries[i].pin1, INPUT);
+    pinMode(rotaries[i].pin2, INPUT);
+    #ifdef ENABLE_PULLUPS
+      digitalWrite(rotaries[i].pin1, HIGH);
+      digitalWrite(rotaries[i].pin2, HIGH);
+    #endif
+  }
+}
+
+unsigned char rotary_process(int _i) {
+   unsigned char pinstate = (digitalRead(rotaries[_i].pin2) << 1) | digitalRead(rotaries[_i].pin1);
+  rotaries[_i].state = ttable[rotaries[_i].state & 0xf][pinstate];
+  return (rotaries[_i].state & 0x30);
+}
+
+void CheckAllEncoders(void) {
+  for (int i=0;i<NUMROTARIES;i++) {
+    unsigned char result = rotary_process(i);
+    if (result == DIR_CCW) {
+      Joystick.setButton(rotaries[i].ccwchar, 1); delay(50); Joystick.setButton(rotaries[i].ccwchar, 0);
+    };
+    if (result == DIR_CW) {
+      Joystick.setButton(rotaries[i].cwchar, 1); delay(50); Joystick.setButton(rotaries[i].cwchar, 0);
+    };
+  }
+}
